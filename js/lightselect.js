183176e4a792c319a80388da288dff0a0f887655
AidenRutzen <AidenRutzen@gmail.com>
2025-03-05 18:28:55 +0000

    Wed, Mar 5, 2025, 10:28 AM -08:00

--- js/lightselect.js
+++ js/lightselect.js
@@ -2,15 +2,15 @@ const element1 = document.getElementById('element1')
 const element2 = document.getElementById('element2')
 
 element1.addEventListener('click', ()=>{
-    element1.innerHTML = "Changing to dark mode..."
+    element1.innerHTML = "Dark Mode"
     element1.style.color = "white"
     element1.style.backgroundColor = "#000000"
-    body.css.backgroundColor = "#000000"
+    document.body.style.backgroundColor = "#000000"
 })
 
 element2.addEventListener('click', ()=>{
-    element2.innerHTML = "Changing to light mode..."
+    element2.innerHTML = "Light Mode"
     element2.style.color = "black"
     element2.style.backgroundColor = "#FFFFFF"
-    body.css.backgroundColor = "#FFFFFF"
+    document.body.style.backgroundColor = "#FFFFFF"
 })
