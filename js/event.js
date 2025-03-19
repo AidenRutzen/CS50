3e62acd71f1edb858f6d3101c564567add94e590
AidenRutzen <AidenRutzen@gmail.com>
2025-03-19 16:53:39 +0000

    Wed, Mar 19, 2025, 9:53 AM -07:00

--- js/event.js
+++ js/event.js
@@ -1,6 +1,7 @@
 const element1 = document.getElementById('element1')
 const element2 = document.getElementById('element2')
 const element3 = document.getElementById('element3')
+const element4 = document.getElementById('element4')
 
 element1.addEventListener('click', ()=>{
     element1.innerHTML = "I've been clicked! <br> Try double clicking me."
@@ -19,9 +20,32 @@ element2.addEventListener('mouseout', ()=>{
     element2.style.color = "white"
     element2.style.backgroundColor = "#BB33BB"
 })
+let positionY = 0;
+let positionX = 0;
 
-console.log("Hi")
 
 document.addEventListener('keyup', (event)=>{
-    console.log(Hello)
+    console.log(event.key)
+
+    if(event.key == 'ArrowDown'){
+        positionY = positionY - 10
+    }
+    else if(event.key == 'ArrowUp'){
+        positionY = positionY + 10
+    }
+    else if(event.key == 'ArrowLeft'){
+        positionX = positionX + 10
+    }
+    else if(event.key == 'ArrowRight'){
+        positionX = positionX - 10
+    }
+    element3.style.top = `{positionY}px`
+    element3.style.right = `{positionX}px`
+})
+
+element4.addEventListener('click', ()=>{
+    element4.style.width = "10px"
+    element4.style.height = "10px"
+    element4.innerHTML = ""
+    element4.style.opacity = .3
 })
