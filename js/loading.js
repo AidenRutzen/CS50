7850049c4d5b7db16c642f67b972e683a19a3a20
AidenRutzen <AidenRutzen@gmail.com>
2025-03-26 15:43:46 +0000

    Wed, Mar 26, 2025, 8:43 AM -07:00

--- js/loading.js
+++ js/loading.js
@@ -1,17 +1,18 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
-let load = 0
 
-let int = setInterval(blurring, 300)
+load = 0;
+
+int = setInterval(blurring, 30)
 
 function blurring() {
-    load = load + 1
-    if (load > 99) {
+    load++
+
+    if(load > 99){
         clearInterval(int)
     }
-
     loadText.innerText = `${load}%`
-    bg.style.filter = `blur(${50 - (load / 2)}px)`
-    loadText.style.opacity = `${1-load/100}`
+    loadText.style.opacity = 1 - load/100
+    bg.style.filter = `blur(${30-(load/100)*30}px)`
 }
