d496cf31c5af0379504436ff7e84b40bd80610fb
AidenRutzen <AidenRutzen@gmail.com>
2025-03-30 16:26:51 -0500

    Update Website

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const element1 = document.getElementById('element1')
+const element2 = document.getElementById('element2')
+const element3 = document.getElementById('element3')
+const element4 = document.getElementById('element4')
+const element5 = document.getElementById('element5')
+const element6 = document.getElementById('element6')
+const element7 = document.getElementById('element7')
+const element8 = document.getElementById('element8')
+
+
+element1.addEventListener('click', ()=>{
+    window.location.href = "https://aidenrutzen.github.io/CS50/event.html"
+})
