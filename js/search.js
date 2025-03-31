daea42e2f30e6a7d29a0f2df438d489092267cd5
AidenRutzen <AidenRutzen@gmail.com>
2025-03-31 12:16:11 -0700

    update

--- js/search.js
+++ js/search.js
@@ -1,6 +1,11 @@
 btn = document.querySelector('.btn')
 search = document.querySelector('.search')
+const element1 = document.querySelector('element1')
 
 btn.addEventListener('click', ()=>{
     search.classList.toggle('active')
+})
+
+element1.addEventListener('click', ()=>{
+    window.location.href = "https://aidenrutzen.github.io/CS50/index.html"
 })
