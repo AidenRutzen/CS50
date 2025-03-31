55636e31207b00b956afae75129c4f39dfd4ea7e
AidenRutzen <AidenRutzen@gmail.com>
2025-03-31 09:28:31 -0700

    update

--- js/event.js
+++ js/event.js
@@ -2,6 +2,7 @@ const element1 = document.getElementById('element1')
 const element2 = document.getElementById('element2')
 const element3 = document.getElementById('element3')
 const element4 = document.getElementById('element4')
+const element5 = document.getElementById('element5')
 const element6 = document.getElementById('element6')
 
 element1.addEventListener('click', ()=>{
