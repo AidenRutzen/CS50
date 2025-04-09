760e04c55238d691d18a54618807fc428aa21dc9
AidenRutzen <AidenRutzen@gmail.com>
2025-04-09 09:58:53 -0700

    update website

---
+++ js/relaxer.js
@@ -0,0 +1,25 @@
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+breathAnimation()
+setInterval(breathAnimation, 7500)
+
+
+function breathAnimation() {
+    text.innerHTML = 'Breath In'
+    container.classList.remove('shrink')
+    container.classList.add('grow')
+
+
+
+    setTimeout(() => {
+        text.innerHTML = 'Hold'
+    }, 3000)
+
+
+    setTimeout(() => {
+        text.innerHTML = 'Breath Out'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 4500)
+}
