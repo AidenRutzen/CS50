064da8a9849d3917c7e9611e86d4ee52a30352b1
AidenRutzen <AidenRutzen@gmail.com>
2025-04-09 17:26:52 +0000

    Wed, Apr 9, 2025, 10:26 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -1,7 +1,8 @@
 const ball = document.createElement('div')
-const ballRadius = 100
+const ballRadius = 30
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
+let ballXPosition = windowWidth/2 - ballRadius
 
 createBall()
 function createBall(){
@@ -11,6 +12,13 @@ function createBall(){
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "green"
     ball.style.position = "absolute"
-    ball.style.top = `${windowHeight/2}px`
-    ball.style.left = `${windowWidth/2}px`
+    ball.style.top = `${windowHeight/2 - ballRadius}px`
+    ball.style.left = `${windowWidth/2 - ballRadius}px`
+}
+
+setInterval(moveBall, 10)
+
+function moveBall() {
+    ballXPosition = ballXPosition + 5
+    ball.style.left = `${ballXPosition}px`
 }
