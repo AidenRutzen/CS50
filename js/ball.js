b431818c748074c9f318ac5955a17ba4cd4aa1ae
AidenRutzen <AidenRutzen@gmail.com>
2025-04-09 17:23:07 +0000

    Wed, Apr 9, 2025, 10:23 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -1,1 +1,16 @@
-const ball = document.
+const ball = document.createElement('div')
+const ballRadius = 100
+const windowHeight = window.innerHeight
+const windowWidth = window.innerWidth
+
+createBall()
+function createBall(){
+    document.body.appendChild(ball)
+    ball.style.height = `${2 * ballRadius}px`
+    ball.style.width = `${2 * ballRadius}px`
+    ball.style.borderRadius = "50%"
+    ball.style.backgroundColor = "green"
+    ball.style.position = "absolute"
+    ball.style.top = `${windowHeight/2}px`
+    ball.style.left = `${windowWidth/2}px`
+}
