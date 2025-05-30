fc9dbc44d048aa8e753c60c08ceb7aa5ebad3b42
AidenRutzen <AidenRutzen@gmail.com>
2025-05-30 16:38:32 +0000

    Fri, May 30, 2025, 9:38 AM -07:00

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const rightSide = document.querySelector('.right-side')
+const leftSide = document.querySelector('.left-side')
+const slidesLength = rightSide.querySelectorAll('div').length
+
+currentSlide = 1
+leftSide.style.top = `-${slidesLength - 1)*100}%`
+
+
+upButton.addEventListener('click', ()=> changeSlide('up'))
+downButton.addEventListener('click', ()=> changeSlide('down'))
+
+function changeSlide(direction) {
+    if(direction == 'up') {
+        leftSide.style.top = leftSide.style.top + 100
+    }
+    else {
+        console.log('down')
+    }
+}
